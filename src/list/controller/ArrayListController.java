911f57da8fb835b95de7ffc01e5356657b905738
cfehlig <cfeh7454@740s-pl-126.local>
2015-10-07 09:55:07 -0600

    added re to the list

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,10 +1,27 @@
 package list.controller;
 
+import java.util.ArrayList;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
 	public void start()
 	{
-		
+		firstWords.add("wefrrfdhgusydfg");
+		firstWords.add("babababa");
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot< firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + " is at spot# " +spot);
+		}
 	}
 
 }
