56397c2e132633b24fe0939a7eb694983732251e
cfehlig <cfeh7454@740s-pl-126.local>
2015-10-07 09:34:15 -0600

    Created a new project, packages, and classes

---
+++ src/list/controller/ArrayListRunner.java
@@ -0,0 +1,11 @@
+package list.controller;
+
+public class ArrayListRunner
+{
+	public static void main(String [] args)
+	{
+		ArrayListController listcontroller= new ArrayListController();
+		listcontroller.start();
+	}
+
+}
